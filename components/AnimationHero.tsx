b6032174129ca74b0c8ff22102f8d92aeeea5545
Alegherix <psao69@gmail.com>
2021-05-26 13:11:57 +0200

    added use of Localstorage to prevent animation from showing each mount

--- components/AnimationHero.tsx
+++ components/AnimationHero.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TriangleAnimation from './TriangleAnimation';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import Wrapper from './Wrapper';
 import { motion } from 'framer-motion';
 
-interface AnimationHeroProps {}
+interface IShownSvg {
+  key: 'SVG';
+  value: boolean;
+}
 
-const AnimationHero: React.FC<AnimationHeroProps> = ({}) => {
+const AnimationHero: React.FC = () => {
+  const [shownSvg, setShownSvg] = useState<boolean>(false);
+
+  const setData = ({ key, value }: IShownSvg) => {
+    localStorage.setItem(key, value.toString());
+  };
+
+  const getPersistedData = (key: string): boolean | null => {
+    const value: boolean | null = JSON.parse(localStorage.getItem(key));
+    return value;
+  };
+
+  // Have to wrap in useEffect to prevent undefined from localStorage,
+  useEffect(() => {
+    setShownSvg(getPersistedData('SVG'));
+  }, []);
+
   return (
     <>
-      <motion.div
-        className="fixed w-screen h-screen bg-evening z-20 px-4"
-        initial={{ y: 0 }}
-        animate={{ y: '-100vh', transition: { delay: 3.2, duration: 1 } }}
-      >
-        <TriangleAnimation />
-      </motion.div>
+      {!shownSvg && (
+        <motion.div
+          className="fixed w-screen h-screen bg-evening z-20 px-4"
+          initial={{ y: 0 }}
+          animate={{ y: '-100vh', transition: { delay: 3.2, duration: 1 } }}
+          onAnimationComplete={() => setData({ key: 'SVG', value: true })}
+        >
+          <TriangleAnimation />
+        </motion.div>
+      )}
+
       <Wrapper>
         <Navbar />
         <Hero />
